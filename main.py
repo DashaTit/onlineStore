f24bee01cea368f04053815c7a929859c73a0c4e
Darua Titova <dachulatitova@gmail.com>
2024-04-09 19:35:48 +0300

    start create basket

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-
+from data import db_session
+from data.users import User
+from forms.users import LoginForm, RegisterForm
+from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 import json
 
 from data_collection import Data_collection
@@ -15,39 +19,79 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 db = SQLAlchemy(app)
 
-class Item(db.Model): #бд
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(500), nullable=False)
+
 
 #routes
 @app.route('/')
 def index():
     with open('result.json', encoding='utf-8') as f:
         templates = json.load(f)
-    items = Item.query.order_by(Item.id).all()
     return render_template('index.html', templates=templates)
 
-# @app.route('/login', methods=["POST", 'GET'])
-# def login():
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         password = request.form['password']
+@app.route('/basket')
+def basket():
+    return render_template('basket.html')
 
-#         item = Item(email=email, password=password)
-#         try:
-#             db.session.add(item)
-#             db.session.commit()
-#             return redirect('/')
-#         except Exception as e:
-#             print(str(e))
-#     else:
-#         return render_template('login.html')
 
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print('login')
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        print('Неправильный логин или пароль')
+        return render_template('login.html',
+                                message="Неправильный логин или пароль",
+                                form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                    form=form,
+                                    message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                    form=form,
+                                    message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            # about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Вы вышли из аккаунта", "success")
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
+    db_session.global_init("bd/blogs.db")
     with app.app_context():
         db.create_all()
     app.run(debug=True)
